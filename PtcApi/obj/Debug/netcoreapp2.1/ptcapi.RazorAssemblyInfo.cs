7d3d894c6425c076ae698265846b1cb3ca47d527
Rick Donalson <rick.donalson@me.com>
2019-04-12 16:29:54 -0400

    commit

--- PtcApi/obj/Debug/netcoreapp2.1/ptcapi.RazorAssemblyInfo.cs
+++ PtcApi/obj/Debug/netcoreapp2.1/ptcapi.RazorAssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.RelatedAssemblyAttribute("ptcapi.Views")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.RelatedAssemblyAttribute("PtcApi.Views")]
 [assembly: Microsoft.AspNetCore.Razor.Hosting.RazorLanguageVersionAttribute("2.1")]
 [assembly: Microsoft.AspNetCore.Razor.Hosting.RazorConfigurationNameAttribute("MVC-2.1")]
 [assembly: Microsoft.AspNetCore.Razor.Hosting.RazorExtensionAssemblyNameAttribute("MVC-2.1", "Microsoft.AspNetCore.Mvc.Razor.Extensions")]
